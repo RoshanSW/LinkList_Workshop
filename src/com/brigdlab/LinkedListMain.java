5158ee6dc842d901e7e4913bd9d2d6bb611a6f39
roshansw <roshanwalzade2@gmail.com>
2022-06-21 09:32:24 +0530

    Linkedlist

---
+++ src/com/brigdlab/LinkedListMain.java
@@ -0,0 +1,21 @@
+package com.brigdlab;
+
+public class LinkedListMain {
+
+	public static void main(String args[]) {
+
+		LinkedList<Integer> LinkedList = new LinkedList<>();
+
+		System.out.println("Append list is: ");
+		LinkedList.add(56);
+		LinkedList.add(30);
+		LinkedList.add(70);
+		LinkedList.print();
+
+		int deletedData = LinkedList.pop();
+		System.out.println();
+		System.out.println("popped head element is: " + deletedData);
+		LinkedList.print();
+	}
+
+}
---
+++ src/com/brigdlab/Node.java
@@ -0,0 +1,17 @@
+package com.brigdlab;
+
+public class Node<E> {
+	
+	E data;
+	
+	Node<E> next;
+	
+	Node(){
+		
+	}
+	
+	Node(E data){
+		this.data = data;
+	}
+
+}
